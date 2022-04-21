1e4050df778bf0558a5e56d36f4f0327261816f9
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2022-04-21 14:28:02 +0700

    update

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,6 @@ export { default as ShareInformationComponent } from './src/components/share-inf
 export { default as WalletCardComponent } from './src/components/wallet-card-component';
 export { default as TransactionListComponent } from './src/components/transaction-list-component';
 export { default as NoWalletComponent } from './src/components/no-wallet-component';
-export * from './src/assets';
+export * from './src/assets/images';
 export * from './src/types';
 export * from './src/model';
