2fb617ba075008c5c098df38116212dc31982aa3
sharmal33 <sharmal@101digital.io>
2022-04-29 11:28:01 +0530

    version update

--- src/components/wallet-card-component/index.tsx
+++ src/components/wallet-card-component/index.tsx
@@ -1,17 +1,32 @@
-import { isEmpty } from 'lodash';
-import React, { ReactNode, useContext, useEffect, useRef, useState } from 'react';
-import { ActivityIndicator, Dimensions, StyleProp, View, ViewStyle } from 'react-native';
-import Carousel from 'react-native-snap-carousel';
-import { ThemeContext } from 'react-native-theme-component';
-import { WalletContext } from '../../context/wallet-context';
-import { Transaction, Wallet } from '../../model';
-import EmptyWalletComponent from '../no-wallet-component';
-import useMergeStyles from './styles';
+import { isEmpty } from "lodash";
+import React, {
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState
+} from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleProp,
+  View,
+  ViewStyle,
+  Text
+} from "react-native";
+import Carousel from "react-native-snap-carousel";
+import { ThemeContext } from "react-native-theme-component";
+import { WalletContext } from "../../context/wallet-context";
+import { Transaction, Wallet } from "../../model";
+import EmptyWalletComponent from "../no-wallet-component";
+import useMergeStyles from "./styles";
 import TransactionCardComponent, {
-  TransactionCardComponentStyles,
-} from './transaction-card-component';
-import WalletItemComponent, { WalletItemComponentStyle } from './wallet-item-component';
-const { width } = Dimensions.get('window');
+  TransactionCardComponentStyles
+} from "./transaction-card-component";
+import WalletItemComponent, {
+  WalletItemComponentStyle
+} from "./wallet-item-component";
+const { width } = Dimensions.get("window");
 
 export type WalletCardComponentProps = {
   style?: WalletCardComponentStyles;
@@ -24,6 +39,7 @@ export type WalletCardComponentProps = {
   onSendMoney: (wallet: Wallet) => void;
   onViewAllTransactions: (wallet: Wallet) => void;
   onTransactionDetails: (transaction: Transaction) => void;
+  children?: ReactNode;
 };
 
 export type WalletCardComponentStyles = {
@@ -45,15 +61,26 @@ const WalletCardComponent = ({
   onViewAllTransactions,
   dateFormat,
   onTransactionDetails,
+  children
 }: WalletCardComponentProps) => {
-  const { colors } = useContext(ThemeContext);
+  const { colors, i18n } = useContext(ThemeContext);
   const styles: WalletCardComponentStyles = useMergeStyles(style);
-  const { transactions, fetchTransactions, wallets, isLoadingWallets } = useContext(WalletContext);
+  const {
+    transactions,
+    fetchTransactions,
+    wallets,
+    isLoadingWallets
+  } = useContext(WalletContext);
+
   // state
   const carouselRef: any = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentWallet, setCurrentWallet] = useState<Wallet | undefined>(undefined);
-  const [_initialWallet, setInitialWallet] = useState<Wallet | undefined>(undefined);
+  const [currentWallet, setCurrentWallet] = useState<Wallet | undefined>(
+    undefined
+  );
+  const [_initialWallet, setInitialWallet] = useState<Wallet | undefined>(
+    undefined
+  );
   const [_initIndex, setInitIndex] = useState<number | undefined>(undefined);
   const _carouselWidth = carouselWidth ?? width;
   const _carouselItemWidth = carouselItemWidth ?? width - 32;
@@ -68,7 +95,9 @@ const WalletCardComponent = ({
   useEffect(() => {
     if (_initialWallet) {
       const initIndex =
-        wallets.findIndex((wallet) => wallet.walletId === _initialWallet.walletId) || 0;
+        wallets.findIndex(
+          wallet => wallet.walletId === _initialWallet.walletId
+        ) || 0;
       if (!_initIndex) {
         setTimeout(() => {
           changeToIndex(initIndex);
@@ -87,7 +116,7 @@ const WalletCardComponent = ({
     if (!isEmpty(wallets)) {
       let focusWallet = wallets[currentIndex];
       const transactionIndex = transactions.findIndex(
-        (ts) => ts.walletId === focusWallet?.walletId
+        ts => ts.walletId === focusWallet?.walletId
       );
       if (focusWallet && transactionIndex === -1) {
         fetchTransactions(focusWallet.walletId, 1);
@@ -96,15 +125,72 @@ const WalletCardComponent = ({
     }
   }, [currentIndex, wallets]);
 
-  if (isEmpty(wallets)) {
+  if (!isEmpty(wallets)) {
     if (isLoadingWallets) {
       return (
         <View style={styles.loadingContainerStyle}>
-          {loadingIndicator ?? <ActivityIndicator color={colors.primaryColor} />}
+          {loadingIndicator ?? (
+            <ActivityIndicator color={colors.primaryColor} />
+          )}
         </View>
       );
     }
-    return <EmptyWalletComponent />;
+
+    if (!children) {
+      return <EmptyWalletComponent />;
+    } else {
+      let item = {
+        availableBalance: 0,
+        currentBalance: 0,
+        bankAccount: {
+          accountId: "string1",
+          accountSubType: "string2",
+          accountHolderName: "string3",
+          accountNumber: "Ongoing verification",
+          bankCode: "string5",
+          countryCode: "PHP",
+          internalProductCategory: "string7",
+          productId: "string8"
+        },
+        currencyCode: "PHP",
+        walletName: "My Pitaka",
+        type: "string11",
+        walletId: "string12",
+        isDefaultWallet: true
+      };
+      return (
+        <View style={styles.containerStyle}>
+          <View style={styles.emptyCarouselContainerStyle}>
+            <WalletItemComponent
+              wallet={item}
+              onAddMoney={() => onAddMoney(item)}
+              onSendMoney={() => onSendMoney(item)}
+              phoneNumber={phoneNumber}
+              style={styles.emptyWalletItemComponentStyle}
+              arrowRightIcon={false}
+              isWithMask={true}
+            />
+          </View>
+          <View style={styles.containerStyleMessage}>
+            <Text style={styles.labelTextStyle}>
+              {i18n?.t("wallet_item_component.lbl_kyc_message1") ??
+                "NOTE: We are currently verifying your account opening request. We will process your request and notify you within 24 hours."}
+            </Text>
+            <Text style={styles.label2TextStyle}>
+              {i18n?.t("wallet_item_component.lbl_kyc_message2") ??
+                "While waiting, you can browse our Financial Literacy content below."}
+            </Text>
+          </View>
+          {children && (
+            <>
+              <Text style={styles.labelTitlStyle}>{"Learn & Grow  🌱"}</Text>
+              <View>{children}</View>
+            </>
+          )}
+        </View>
+      );
+    }
+    // return <EmptyWalletComponent />;
   }
 
   return (
@@ -132,8 +218,8 @@ const WalletCardComponent = ({
           itemWidth={_carouselItemWidth}
           inactiveSlideScale={1}
           loop={false}
-          activeSlideAlignment='center'
-          layout='default'
+          activeSlideAlignment="center"
+          layout="default"
           onSnapToItem={(index: number) => {
             if (_initialWallet) {
               setInitialWallet(undefined);
@@ -153,6 +239,7 @@ const WalletCardComponent = ({
           style={styles.transactionCardComponentStyle}
         />
       )}
+      {children && <View>{children}</View>}
     </View>
   );
 };
--- src/components/wallet-card-component/styles.ts
+++ src/components/wallet-card-component/styles.ts
@@ -1,17 +1,79 @@
-import { defaultsDeep } from 'lodash';
-import { StyleSheet } from 'react-native';
-import { WalletCardComponentStyles } from '.';
+import { defaultsDeep } from "lodash";
+import { StyleSheet } from "react-native";
+import { WalletCardComponentStyles } from ".";
+import { ThemeContext } from "react-native-theme-component";
+import { useContext } from "react";
 
-const useMergeStyles = (style?: WalletCardComponentStyles): WalletCardComponentStyles => {
+const useMergeStyles = (
+  style?: WalletCardComponentStyles
+): WalletCardComponentStyles => {
+  const { fonts, colors } = useContext(ThemeContext);
+
   const defaultStyles: WalletCardComponentStyles = StyleSheet.create({
     containerStyle: {
-      flex: 1,
+      flex: 1
     },
     loadingContainerStyle: {
       height: 173,
-      alignItems: 'center',
-      justifyContent: 'center',
+      alignItems: "center",
+      justifyContent: "center"
     },
+    emptyCarouselContainerStyle: {
+      marginHorizontal: 25
+    },
+    emptyWalletItemComponentStyle: {
+      accountNumberStyle2: {
+        fontSize: 12,
+        color: "#7F7B82",
+        flex: 1
+      },
+      accountNumberStyle: {
+        // fontFamily: fonts.bold,
+        fontSize: 14,
+        color: "#7F7B82",
+        marginRight: 3,
+        textDecorationLine: "none"
+      }
+    },
+    containerStyleMessage: {
+      marginHorizontal: 25,
+      backgroundColor: "#E7DBF5",
+      marginVertical: 16,
+      borderRadius: 8,
+      elevation: 2,
+      shadowColor: "grey",
+      shadowOffset: {
+        width: 1,
+        height: 1
+      },
+      shadowOpacity: 0.1,
+      shadowRadius: 1,
+      margin: 1,
+      // paddingBottom: 22,
+      padding: 15
+    },
+    labelTextStyle: {
+      fontSize: 10,
+      lineHeight: 10,
+      color: "#5E0CBC",
+      fontWeight: "400",
+      width: 280
+    },
+    label2TextStyle: {
+      fontSize: 10,
+      lineHeight: 10,
+      color: "#5E0CBC",
+      fontWeight: "400",
+      marginTop: 20,
+      width: 280
+    },
+    labelTitlStyle: {
+      fontFamily: fonts.bold,
+      fontSize: 16,
+      lineHeight: 36,
+      color: colors.primaryTextColor,
+      paddingLeft: 20
+    }
   });
 
   return defaultsDeep(style, defaultStyles);
