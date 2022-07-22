e8e445698c9880036c292914a2a694b13e59e253
khoi anh <khoi@101digital.com>
2022-07-22 10:12:09 +0700

    complete ui

--- src/components/crypto-components/crypto-transaction-posting-component/types.ts
+++ src/components/crypto-components/crypto-transaction-posting-component/types.ts
@@ -4,7 +4,7 @@ export type CryptoTransactionPostingComponentProps = {
   style?: CryptoTransactionPostingComponentStyles;
   props?: { 
     onBackToDashboard?: () => void,
-    onBackToTranferIn?: () => void,
+    onBackToTransferIn?: () => void,
     onGoToHelpCenter?: () => void
   };
 };
@@ -12,4 +12,32 @@ export type CryptoTransactionPostingComponentProps = {
 export type CryptoTransactionPostingComponentStyles = {
   safeArea?: StyleProp<ViewStyle>;
   container?: StyleProp<ViewStyle>;
+  containerCenter?: StyleProp<ViewStyle>;
+  content?: StyleProp<ViewStyle>;
+  title?: StyleProp<TextStyle>;
+  subTitleWrapper?: StyleProp<ViewStyle>;
+  circleProgressWrapper?: StyleProp<ViewStyle>;
+  btnTransparent?: StyleProp<ViewStyle>;
+  labelBtnTransaprent?: StyleProp<TextStyle>;
+  completedTextColor?: StyleProp<TextStyle>;
+  titleSuccess?: StyleProp<TextStyle>;
+  subTitle?: StyleProp<TextStyle>;
+  subTitleSuccess?: StyleProp<TextStyle>;
+  contentSuccess?: StyleProp<ViewStyle>;
+  subTitleSuccessWrapper?: StyleProp<ViewStyle>;
+  btnActionsWrapper?: StyleProp<ViewStyle>;
+  logoContainer?: StyleProp<ViewStyle>;
+  rowBetween?: StyleProp<ViewStyle>;
+  infoTitle?: StyleProp<TextStyle>;
+  infoSubTitle?: StyleProp<TextStyle>;
+  containerFailed?: StyleProp<ViewStyle>;
+  errorContentWrapper?: StyleProp<ViewStyle>;
+  columnBetween?: StyleProp<ViewStyle>;
+  errorTitleWrapper?: StyleProp<ViewStyle>;
+  iconErrorWrapper?: StyleProp<ViewStyle>;
+  errorMessageWrapper?: StyleProp<ViewStyle>;
+  rowErrorBetween?: StyleProp<ViewStyle>;
+  statusLabel?: StyleProp<TextStyle>;
+  errorMessageLabel?: StyleProp<TextStyle>;
+  errorInfoTitleColor?: StyleProp<TextStyle>;
 };
