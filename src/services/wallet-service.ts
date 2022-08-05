daec91a5771737be7a6a36229071ef2327f755b1
khoi anh <khoi@101digital.com>
2022-08-05 12:38:02 +0700

    update version

--- src/services/wallet-service.ts
+++ src/services/wallet-service.ts
@@ -2,6 +2,8 @@ type WalletClient = {
   walletClient: any;
   financialClient: any;
   contentTemplateClient: any;
+  exchangeRateClient: any;
+  countryInformationClient: any;
 };
 
 export class WalletService {
@@ -10,6 +12,8 @@ export class WalletService {
   private _walletClient?: any;
   private _financialClient?: any;
   private _contentTemplateClient?: any;
+  private _exchangeRateClient?: any;
+  private _countryInformationClient?: any;
 
   constructor() {
     if (WalletService._instance) {
@@ -28,6 +32,8 @@ export class WalletService {
     this._walletClient = clients.walletClient;
     this._financialClient = clients.financialClient;
     this._contentTemplateClient = clients.contentTemplateClient;
+    this._exchangeRateClient = clients.exchangeRateClient;
+    this._countryInformationClient = clients.countryInformationClient;
   };
 
   getWallets = async () => {
@@ -36,19 +42,6 @@ export class WalletService {
         params: {
           pageSize: 0,
           pageNumber: 0,
-        },
-      });
-      return response.data;
-    } else {
-      throw new Error('Wallet Client is not registered');
-    }
-  };
-
-  getWalletsByBankId = async (bankId:string) => {
-    if (this._walletClient) {
-      const response = await this._walletClient.get('wallets', {
-        params: {
-          bankId: bankId,
         },
       });
       return response.data;
@@ -66,14 +59,21 @@ export class WalletService {
     }
   };
 
-  linkBankAccount = async (bankId: string, consentId: string, accountIds?: string[]) => {
+  linkBankAccount = async (
+    bankId: string,
+    consentId: string,
+    accountIds?: string[]
+  ) => {
     if (this._walletClient) {
-      const response = await this._walletClient.post('wallets/link-bank-accounts', {
-        bankId,
-        consentId,
-        accountIds,
-        async: true,
-      });
+      const response = await this._walletClient.post(
+        'wallets/link-bank-accounts',
+        {
+          bankId,
+          consentId,
+          accountIds,
+          async: true,
+        }
+      );
       return response.data;
     } else {
       throw new Error('Wallet Client is not registered');
@@ -143,20 +143,90 @@ export class WalletService {
     }
   };
 
-  getCryptoTC = async (templateName:string,appId:string,entityId:string,format:string) => {
+  getCryptoTC = async (
+    templateName: string,
+    appId: string,
+    entityId: string,
+    format: string
+  ) => {
     if (this._contentTemplateClient) {
-      const response = await this._contentTemplateClient.post('contents',{
-      	appId: appId,
-      	documentFormat: format,
-      	entityId: entityId,
-      	templateName: templateName,
-        data: {}
+      const response = await this._contentTemplateClient.post('contents', {
+        appId: appId,
+        documentFormat: format,
+        entityId: entityId,
+        templateName: templateName,
+        data: {},
       });
       return response.data;
     } else {
-      console.log('eeeeeeeeeeeeee');
-
       throw new Error('Content Template Client is not registered');
+    }
+  };
+
+  getCurrenciesExchangeRate = async (
+    pageNum?: number,
+    pageSize?: number,
+    toCurrency?: string
+  ) => {
+    console.log('getCurrenciesExchangeRate', this._exchangeRateClient);
+    if (this._exchangeRateClient) {
+      const response = await this._exchangeRateClient.get(
+        'currencies/exchange-rates',
+        {
+          params: {
+            pageNum: pageNum,
+            pageSize: pageSize,
+            toCurrency: toCurrency,
+          },
+        }
+      );
+      console.log('response', response);
+      return response.data;
+    } else {
+      throw new Error('Exchange rate client service is not registered');
+    }
+  };
+
+  getCurrenciesHistoricalExchangeRate = async (
+    updateAtFrom?: string,
+    fromCurrency?: string,
+    toCurrency?: string,
+    pageNum?: number,
+    pageSize?: number
+  ) => {
+    console.log('getCurrenciesExchangeRate', this._exchangeRateClient);
+    if (this._exchangeRateClient) {
+      const response = await this._exchangeRateClient.get(
+        'currencies/historical-exchange-rates',
+        {
+          params: {
+            pageNum,
+            pageSize,
+            fromCurrency,
+            toCurrency,
+            updateAtFrom: updateAtFrom,
+          },
+        }
+      );
+      console.log('response', response.data);
+      return response.data;
+    } else {
+      throw new Error(
+        'Historical exchange rate client service is not registered'
+      );
+    }
+  };
+
+  getListCurrency = async () => {
+    if (this._countryInformationClient) {
+      const response = await this._countryInformationClient.get(
+        'currencies?currencyType=CRYPTO'
+      );
+      return response.data;
+    } else {
+      throw new Error(
+        'Historical exchange rate client service is not registered'
+      );
     }
   };
 }
